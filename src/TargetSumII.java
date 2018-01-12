4c59a5a93213ba439a411e1df519e5c42922a657
mchiu002 <mchiu002@fiu.edu>
2018-01-12 17:47:31 -0600

    Update TargetSumII.java

--- src/TargetSumII.java
+++ src/TargetSumII.java
@@ -21,3 +21,26 @@ public static List<int[]> targetSumII(int[] array, int target){
       }  
   return results;
 }
+
+/*
+optimized solution
+time complexity: O(n^2)
+*/
+public static List<int[]> targetSumII(int[] array, int target){
+  HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+  List<int[]> results = new ArrayList<int[]>();
+
+      for(int i = 0; i< array.length; i++){
+        for(int j = i+1; j< array.length; j++){
+          if(map.containsKey(target-(array[i]+ array[j]))){
+            int[] result = new int[3];
+            result[0] = array[i];
+            result[1] = array[j];
+            result[2] = target - (array[i] + array[j]);
+            results.add(result);
+          }
+        }
+        map.put(array[i], 1);
+      }
+  return results;
+}
