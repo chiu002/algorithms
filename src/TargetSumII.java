82613ad803abbc52f7debb4a23bc70fd3338adf0
mchiu002 <mchiu002@fiu.edu>
2018-01-12 16:11:39 -0600

    Update TargetSumII.java

--- src/TargetSumII.java
+++ src/TargetSumII.java
@@ -1,1 +1,23 @@
-
+/*
+brute force
+time complexity: O(n^3)
+*/
+public static List<int[]> targetSumII(int[] array, int target){
+    if(array.length < 3)
+      return null;
+      List<int[]> results = new ArrayList<int[]>();
+      for(int i = 0; i< array.length; i++){
+        for(int j = i+1; j< array.length; j++){
+          for(int k = j+1; k < array.length; k++ ){
+            if(array[i] + array[j] + array[k] == target){
+              int[] result = new int[3];
+              result[0] = array[i];
+              result[1] = array[j];
+              result[2] = array[k];
+              results.add(result);
+            }
+          }
+        }
+      }  
+  return results;
+}
